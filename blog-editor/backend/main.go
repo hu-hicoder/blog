5177d0c6a45864528065fe63811f3be9b3650bff
Masanori Tani <metro@uta8a.org>
2021-11-02 06:33:40 +0000

    update

--- blog-editor/backend/main.go
+++ blog-editor/backend/main.go
@@ -5,9 +5,27 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+// type DBConfig struct {
+// 	User
+// }
+type Article struct {
+	gorm.Model // ID, created_at, updated_at, deleted_at
+	Title      string
+	Author     string
+	Content    string
+}
+
 func main() {
+	db, err := gorm.Open(mysql.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Article{})
+
 	// Echo instance
 	e := echo.New()
 
